a04b4293be739d887967b3a4d3717cd62f5c04bf
Eraraya Morenzo Muten <morenzomuten@gmail.com>
2022-03-20 06:36:35 +0700

    binary, hex, unsigned, and signed 2C result

--- uts/nomor_1/binary_operation.c
+++ uts/nomor_1/binary_operation.c
@@ -2,9 +2,9 @@
 *Hari dan Tanggal : Kamis, 17 Maret 2022
 *Nama (NIM)       : Eraraya Morenzo Muten (18320003)
 *Nama File        : binary_operation.c
-*Deskripsi        : Program menerima matriks sebesar m kali n,
-*					melakukan transpos konjugat, memeriksa sifat
-*					matriks, dan menampilkan sifat tersebut.
+*Deskripsi        : Program NIM kemudian melakukan
+*					berbagai operasi terhadap bilangan
+*					biner dari NIM.
 */
 
 #include <stdio.h>
@@ -13,7 +13,7 @@
 #include <math.h>
 
 void decToBin(int dec, int* bin, int len){
-	// dengan menggunakan division method
+	// Menggunakan division method
 	int remain, i = 1, j = len-1;
 
 	while (dec != 0) {
@@ -27,6 +27,7 @@ void decToBin(int dec, int* bin, int len){
 }
 
 int binToDec(int bin){
+	// Menggunakan basis 10
 	int dec = 0;
 	int remain, i = 0;
 	
@@ -40,21 +41,64 @@ int binToDec(int bin){
 	return dec;
 }
 
-/*
-const char* binToHex(int bin){
-	char hex[2+1];
+int matrixToInt(int* matrix){
+	// Mengubah bilangan biner bentuk matriks
+	// menjadi bentuk integer
+	int bin = 0;
+	int i;
 	
+	for(i=0; i<8; ++i){
+		bin += matrix[i] * pow(10, 7-i);
+	}
+	
+	return bin;
+}
+
+int binToHex(int bin){
+	// Menggunakan basis 16 dan
+	// format hexadecimal di C
+	int hex = 0;
+	int remain, i = 1;
+	
+	while (bin != 0){
+      remain = bin % 10;
+      hex = hex + remain * i;
+      i = i * 2;
+      bin = bin / 10;
+   }
 	
 	return hex;
 }
 
-int binToSig(int bin){
-	int sig;
+int binToSigned(int* bin){
+	int sig = 0;
+	int i;
 	
+	for(i=1; i<8; ++i){
+		sig += bin[i] * pow(10, 7-i);
+	}
 	
-	return sig;
+	if(bin[0] == 1){
+		return -1*binToDec(sig);
+	} else if(bin[0] == 0){
+		return binToDec(sig);
+	}
 }
-*/
+
+void printHasil(int* hasil, int i){
+	printf("%d: \n", i);
+	printf("Binary: ");
+	for(i=0; i<8; ++i){
+		printf("%d", hasil[i]);
+	}
+	printf("\n");
+	printf("Hexadecimal: %X\n", binToHex(matrixToInt(hasil)));
+	printf("Unsigned: %d\n", binToDec(matrixToInt(hasil)));
+	printf("Signed: %d\n", binToSigned(hasil));
+	printf("\n");
+	
+	return;
+}
 
 int main()
 {
@@ -65,8 +109,6 @@ int main()
 	int A;
 	int hasil[8];
 	int y[8];
-	
-	//signed x, y;
 	
 	// Meminta input NIM
 	printf("Masukkan NIM (8 digit): ");
@@ -79,9 +121,6 @@ int main()
 	strncat(nimPart, &nim[6], 1);
 	strncat(nimPart, &nim[7], 1);
 	
-	// debugging
-	// printf("nimPart: %s\n", nimPart);
-	
 	// Mengisi matriks biner
 	for(i=0; i<17+1;++i){
 		AFull[i] = -1;
@@ -90,21 +129,10 @@ int main()
 	// Mendapatkan binary dari NIM
 	decToBin(atoi(nimPart), AFull, 17);
 	
-	// debugging
-	/*
-	for(i=0; i<17;++i){
-		printf("%d", AFull[i]);
-	}
-	printf("\n");
-	*/
-	
 	// Mengambil last significant byte dari NIM
 	for(i=0; i<8; ++i){
 		A += AFull[i+9] * pow(10, 7-i);
 	}
-	
-	// debugging
-	//printf("%d", A);
 	
 	// Operasi 1: (A & y) ^ x 
 	// x = 1010 0101 = 165
@@ -112,11 +140,23 @@ int main()
 	decToBin(((binToDec(A) & 239) ^ 165), hasil, 8);
 	
 	// Mencetak Hasil Binary
-	printf("1: ");
-	for(i=0; i<8;++i){
-		printf("%d", hasil[i]);
-	}
-	printf("\n");
+	printHasil(hasil, 1);
+
+	// Operasi 2: (~(y >> 4) && x) + A 
+	// x = 1101 1011 = 219
+	// y = 1111 0011 = 243
+	decToBin((1+binToDec(A)), hasil, 8);
+	
+	// Mencetak Hasil Binary
+	printHasil(hasil, 2);
+	
+	// Operasi 3: ((x >> 2) | y) * A 
+	// x = 0010 1111 = 47
+	// y = 1010 0101 = 165
+	decToBin((((47 >> 2) | 165) * binToDec(A)), hasil, 8);
+	
+	// Mencetak Hasil Binary
+	printHasil(hasil, 3);
 	
     return 0;
 }
